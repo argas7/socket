d70adbccc5d60a68e0b337914c04bf482d20ce5f
argas7 <argas@cin.ufpe.br>
2020-07-14 03:03:00 -0300

    finish server

--- index.js
+++ index.js
@@ -6,8 +6,22 @@ const app = express();
 const server = http.createServer(app);
 const io = socket(server);
 
+const users = {};
+
 io.on('connection', (socket) => {
-  console.log('Hy, I am here');
+  if (!users[socket.id]) users[socket.id] = socket.id;
+
+  socket.emit('myID', socket.id);
+  io.sockets.emit('allUsers', users);
+  socket.on('disconnect', () => delete users[socket.id]);
+
+  socket.on('callUser', (data) => {
+    io.to(data.userToCall).emit('call', { signal: data.signalData, from: data.from });
+  });
+
+  socket.on('acceptCall', (data) => {
+    io.to(data.to).emit('callAccepted', data.signal);
+  });
 });
 
 server.listen(3001, () => console.log('Running on PORT:3001'));
